Fix for hour angle calculation
- improve formatting of table

spot/plugins/Targets.py
@@ -158,8 +158,8 @@ class Targets(GingaPlugin.LocalPlugin):
                         ('Moon Sep', 'moon_sep'),
                         ('RA', 'ra'),
                         ('DEC', 'dec'),
-                        #('Eq', 'equinox'),
-                        #('Comment', 'comment'),
+                        ('Eq', 'equinox'),
+                        ('Comment', 'comment'),
                         ]
 
         # the solar system objects
@@ -514,7 +514,7 @@ class Targets(GingaPlugin.LocalPlugin):
 
             # add additional columns
             _addl_str_cols = np.asarray([(tgt.get('color', self.settings['color_normal']),
-                                          tgt.category)
+                                          tgt.category, tgt.get('comment', ''))
                                          for tgt in self.full_tgt_list]).T
             _addl_bool_cols = np.array([(tgt in self.tagged,
                                          tgt.metadata.get('is_ref', True))
@@ -522,6 +522,7 @@ class Targets(GingaPlugin.LocalPlugin):
                                        dtype=bool).T
             dct_all['color'] = _addl_str_cols[0]
             dct_all['category'] = _addl_str_cols[1]
+            dct_all['comment'] = _addl_str_cols[2]
             dct_all['tagged'] = _addl_bool_cols[0]
             dct_all['is_ref'] = _addl_bool_cols[1]
 
@@ -624,7 +625,7 @@ class Targets(GingaPlugin.LocalPlugin):
                        equinox=eq,
                        comment=row.get('comment', ''),
                        category=category)
-            t.set(is_ref=row.get('IsRef', None))
+            t.set(is_ref=row.get('IsRef', True))
             new_targets.append(t)
 
         if not merge:
@@ -647,7 +648,13 @@ class Targets(GingaPlugin.LocalPlugin):
 
     def process_csv_file_for_targets(self, csv_path):
         tgt_df = pd.read_csv(csv_path)
-        tgt_df['IsRef'] = [True for i in range(len(tgt_df))]
+        if 'Equinox' not in tgt_df:
+            tgt_df['Equinox'] = [2000.0] * len(tgt_df)
+        if 'IsRef' not in tgt_df:
+            tgt_df['IsRef'] = [True] * len(tgt_df)
+        if 'Comment' not in tgt_df:
+            tgt_df['Comment'] = [os.path.basename(csv_path)] * len(tgt_df)
+
         merge = self.w.merge_targets.get_state()
         category = csv_path if not merge else "Targets"
         self.add_targets(category, tgt_df, merge=merge)
@@ -680,16 +687,17 @@ class Targets(GingaPlugin.LocalPlugin):
 
         # process into Target object list
         new_targets = []
+        comment = os.path.basename(ope_file)
         for tgt_info in tgt_res.tgt_list_info:
             objname = tgt_info.objname
             ra_str = tgt_info.ra
             dec_str = tgt_info.dec
             eq_str = tgt_info.eq
             is_ref = tgt_info.is_referenced
-            new_targets.append((objname, ra_str, dec_str, eq_str, is_ref))
+            new_targets.append((objname, ra_str, dec_str, eq_str, comment, is_ref))
 
         tgt_df = pd.DataFrame(new_targets,
-                              columns=["Name", "RA", "DEC", "Equinox", "IsRef"])
+                              columns=["Name", "RA", "DEC", "Equinox", "Comment", "IsRef"])
         merge = self.w.merge_targets.get_state()
         category = ope_file if not merge else "Targets"
         self.add_targets(category, tgt_df, merge=merge)
@@ -698,7 +706,7 @@ class Targets(GingaPlugin.LocalPlugin):
     def targets_to_table(self, tgt_df):
         tree_dict = OrderedDict()
         for idx, row in tgt_df.iterrows():
-            is_ref = row.get('is_ref', None)
+            is_ref = row.get('is_ref', True)
             if self.w.list_prm_targets.get_state() or is_ref:
                 dct = tree_dict.setdefault(row.category, dict())
                 tagged = row['tagged']
@@ -716,17 +724,16 @@ class Targets(GingaPlugin.LocalPlugin):
                     name=row['name'],
                     ra=wcs.ra_deg_to_str(row.ra_deg),
                     dec=wcs.dec_deg_to_str(row.dec_deg),
-                    #equinox=("%6.1f" % row.equinox),
-                    az_deg=("% 4d" % int(round(az_deg))),
-                    alt_deg=("% 3d" % int(round(row.alt_deg))),
-                    parang_deg=("% 3d" % int(row.pang_deg)),
-                    ha=("% 6.2f" % (np.degrees(row.ha) / 15)),
+                    equinox="{:>6.1f}".format(row.equinox),
+                    az_deg="{:>+4d}".format(int(round(az_deg))),
+                    alt_deg="{:>3d}".format(int(round(row.alt_deg))),
+                    parang_deg="{:>3d}".format(int(row.pang_deg)),
+                    ha="{:>+6.2f}".format(np.degrees(row.ha) / 15),
+                    ad="{:>3.1f}".format(np.degrees(calc_ad) * 3600),
                     icon=self._get_dir_icon(row),
-                    airmass=("% 5.2f" % row.airmass),
-                    moon_sep=("% 3d" % int(round(row.moon_sep))),
-                    # TODO
-                    #comment=row.comment,
-                    ad=("% .1f" % (np.degrees(calc_ad) * 3600)))
+                    airmass="{:>5.2f}".format(row.airmass),
+                    moon_sep="{:>3d}".format(int(round(row.moon_sep))),
+                    comment=row.comment)
 
         # save and restore selection after update
         # NOTE: calling set_tree() will trigger the target_selection_cb,

spot/util/calcpos.py
@@ -8,6 +8,7 @@ import numpy as np
 from datetime import datetime, time, timedelta
 from dateutil import tz
 import dateutil.parser
+import pandas as pd
 
 # set up download directory for files
 from ginga.util.paths import ginga_home
@@ -16,7 +17,7 @@ if not os.path.isdir(datadir):
     os.mkdir(datadir)
 
 import erfa
-from astropy.coordinates import SkyCoord, Latitude, Longitude
+from astropy.coordinates import SkyCoord, Latitude, Longitude, Angle
 from astropy.time import Time
 from astropy import units as u
 from astropy.config import set_temp_cache
@@ -532,6 +533,12 @@ class Body(object):
         self.equinox = equinox
         self.comment = comment
 
+    def _get_epoch(self, eq):
+        if isinstance(eq, str):
+            if eq.startswith('J'):
+                eq = eq[1:]
+        return float(eq)
+
     def _get_coord(self):   # noqa
         # vector construction, if the value passed is an array
         if isinstance(self.ra, np.ndarray):
@@ -543,9 +550,8 @@ class Body(object):
             else:
                 ra_deg, dec_deg = self.ra.astype(float), self.dec.astype(float)
 
-            #epoch = self.equinox
-            epoch = np.array([2000.0] * len(ra_deg))
-            import pandas as pd
+            #epoch = np.array([self._get_epoch(eq) for eq in self.equinox])
+            epoch = [2000.0] * len(self.ra)
             df = pd.DataFrame(dict(names=self.name, ra_degrees=ra_deg,
                                    ra_hours=ra_deg / 15.0,
                                    dec_degrees=dec_deg,
@@ -559,8 +565,7 @@ class Body(object):
                 ra_deg, dec_deg = coord.ra.degree, coord.dec.degree
             else:
                 ra_deg, dec_deg = float(self.ra), float(self.dec)
-            #coord = Star(ra_hours=ra_deg / 15.0, dec_degrees=dec_deg,
-            #             epoch=self.equinox)
+            # TODO: equinox
             coord = Star(ra_hours=ra_deg / 15.0, dec_degrees=dec_deg)
 
         return coord
@@ -615,6 +620,7 @@ class CalculationResult(object):
         self._last = None
         self._ra = None
         self._dec = None
+        self._eq = None
         self._alt = None
         self._az = None
         self._ha = None
@@ -658,7 +664,9 @@ class CalculationResult(object):
 
     @property
     def equinox(self):
-        return self.body.equinox
+        if self._eq is None:
+            self._calc_radec()
+        return self._eq
 
     @property
     def alt(self):
@@ -721,39 +729,46 @@ class CalculationResult(object):
         """Return Greenwich Mean Sidereal Time in radians."""
         if self._gmst is None:
             # NOTE:obstime.gmst is in hours
-            self._gmst = np.radians(self.obstime.gmst * 15.0)
-        return self._gmst
+            self._gmst = self.obstime.gmst
+        return np.radians(self._gmst * 15.0)
 
     @property
     def gast(self):
         """Return Greenwich Apparent Sidereal Time in radians."""
         if self._gast is None:
             # NOTE:obstime.gast is in hours
-            self._gast = np.radians(self.obstime.gast * 15.0)
-        return self._gast
+            self._gast = self.obstime.gast
+        return np.radians(self._gast * 15.0)
 
     @property
     def lmst(self):
         """Return Local Mean Sidereal Time in radians."""
         if self._lmst is None:
             # NOTE:obstime.gmst is in hours
-            self._lmst = np.radians(self.obstime.gmst * 15.0 + self.observer.lon_deg)
-        return self._lmst
+            _lmst = self.obstime.gmst + self.observer.lon_deg / 15.0
+            # normalize to 24 hour time
+            self._lmst = np.fmod(_lmst + 24.0, 24.0)
+        return np.radians(self._lmst * 15)
 
     @property
     def last(self):
         """Return Local Apparent Sidereal Time in radians."""
         if self._last is None:
             # NOTE:obstime.gast is in hours
-            self._last = np.radians(self.obstime.gast * 15.0 + self.observer.lon_deg)
+            _last = self.obstime.gast + self.observer.lon_deg / 15.0
+            # normalize to 24 hour time
+            self._last = np.fmod(_last + 24.0, 24.0)
         return self._last
 
     @property
     def ha(self):
         """Return the Hour Angle in radians."""
         if self._ha is None:
-            self._ha = self.lmst - self.ra
-        return self._ha
+            lmst = self.lmst     # force calculation of self._lmst
+            _ha = Angle(self._lmst - self.ra_deg / 15.0, unit=u.hour)
+            _ha.wrap_at(12 * u.hour, inplace=True)
+            self._ha = _ha
+        return self._ha.rad
 
     @property
     def pang(self):
@@ -876,6 +891,8 @@ class CalculationResult(object):
         if hasattr(coord, 'ra'):
             self._ra, self._dec = coord.ra, coord.dec
             self._ra_deg, self._dec_deg = coord.ra.hours * 15.0, coord.dec.degrees
+            # TODO
+            self._eq = 2000.0
         else:
             # moons, planets, etc.
             # Need to calculate the apparent ra/dec from the azalt
@@ -884,6 +901,8 @@ class CalculationResult(object):
                                                                           az_degrees=self.az_deg)
             self._ra, self._dec, distance = apparent.radec()
             self._ra_deg, self._dec_deg = self._ra.hours * 15.0, self._dec.degrees
+            # TODO
+            self._eq = 2000.0
 
     def _calc_altaz(self):
         coord = self.body._get_coord()
@@ -905,6 +924,7 @@ class CalculationResult(object):
         if columns is None:
             return dict(name=self.name, ra=self.ra, ra_deg=self.ra_deg,
                         dec=self.dec, dec_deg=self.dec_deg, az=self.az,
+                        equinox=self.equinox,
                         az_deg=self.az_deg, alt=self.alt, alt_deg=self.alt_deg,
                         lt=self.lt, ut=self.ut, jd=self.jd, mjd=self.mjd,
                         gast=self.gast, gmst=self.gmst, last=self.last,